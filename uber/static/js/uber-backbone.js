9571843dd396961d6a3a33f301982bcccf7b61f6
cooncesean <cooncesean@gmail.com>
2013-10-22 15:47:48 -0700

    Separating the 'autocompleter', 'map' and 'app' logic into separate, encapsulated views.

--- uber/static/js/uber-backbone.js
+++ uber/static/js/uber-backbone.js
@@ -16,6 +16,7 @@ $(function(){
   });
 
 
+
   // COLLECTIONS (`MovieLocations` is defined and bootstrapped in `index.html`)
   var SelectedMoviesList = Backbone.Collection.extend({
     model: MovieLocation
@@ -24,99 +25,33 @@ $(function(){
   // **SelectedMovies** will be a list of movies selected from the autocompleter
   var SelectedMovies = new SelectedMoviesList;
 
-  // ROUTER
-  var Workspace = Backbone.Router.extend({
-    routes: {
-      "movies":               "movies",          // #movies/
-      "movies/:query":        "movies",          // #movies/kiwis
-      "search_history":       "search_history",  // #search_history
-    },
-
-    // Fetches all movies (or a filtered subset)
-    movies: function(query) {
-      console.log('movies');
-      console.log(query);
-    },
-
-    // Manipulates the user's search history
-    search_history: function() {
-      console.log('search_history')
-    }
-  });
 
 
-  // VIEWS
-  // Top-level piece of UI.
-  var AppView = Backbone.View.extend({
-    el: $("#container"),
+  // MAPVIEW /////////////////////////////////////////////////////////////////
+  // Manages UI around the single Google Map binding.
+  /////////////////////////////////////////////////////////////////////////////
+  var MapView = Backbone.View.extend({
+    el: $('#map-canvas'),
+    markers: [],
 
-    // Delegated events for searching for specific movies,
-    // and selecting autocompleted results
-    events: {
-      "keypress #movie-title":  "searchForMovie",
-      "click #autocompleter-results": "selectResult",
-    },
-
-    // At initialization we bind to the relevant events on the `autocomplete input`
-    // Loading any all movie locations from the API.
+    // Init the map on the page, binding it to #map-canvas
     initialize: function() {
-
-      // Bind autocompleter actions
-      this.autocompleter = this.$("#autocompleter");
-      this.autocompleter.on('keypress', this.handleAutoComplete);
-
-      // Bind map actions and initialize the map
-      this.map_dom = this.$("#map-canvas");
-      this.markers = [];
-      this.initializeMap();
-    },
-
-    foo: function(collection, response, options){
-      console.log(collection);
-    },
-
-    // Re-rendering the App just means
-    //   1. Updating the markers on the map
-    //   2. Clearing the auto-completer
-    //   3. Appending their selected result to the user's `search history`
-    //   4. Update the 'movie info' dom elements on the side bar
-    render: function() {
-      console.log('rendering')
-      this.map.updateMarkers();
-    },
-
-    // Make an async request to get a matching list of movies
-    // when the autocompleter is receives input
-    handleAutoComplete: function(){
-      var query = $(this).val();
-      if(query.length < 2)
-        return;
-      console.log(query);
-
-
-    },
-
-    // Google Maps initialization
-    initializeMap: function(){
       // Google config
       var mapOptions = {
-        center: new google.maps.LatLng(37.7533, -122.4173),
+        center: new google.maps.LatLng(37.7533, -122.4173), // SF
         zoom: 11,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
-      this.map = new google.maps.Map(
+      this.googleMap = new google.maps.Map(
         document.getElementById("map-canvas"),
         mapOptions
       );
       var infowindow = new google.maps.InfoWindow();
       var self = this;
 
-      // Iterate over each movie in the collection and create a marker
-      $.each(MovieLocations.models, function(){
-        self.createMarker(
-          self.map,
-          this
-        );
+      // Createa a map marker for each location in the `movieLocations` collection
+      $.each(movieLocations.models, function(){
+        self.createMarker(self.googleMap, this);
       });
     },
 
@@ -154,10 +89,78 @@ $(function(){
         markers.pop().setMap(null);
       }
     }
+  });
+  var mapView = new MapView;
+
+  // AUTOCOMPLETEVIEW /////////////////////////////////////////////////////////
+  // This view manages the autocompleter as well as as the selection list
+  // that renders the autocompleted results.
+  /////////////////////////////////////////////////////////////////////////////
+  var AutoCompleterView = Backbone.View.extend({
+    el: $('#autocompleter'),
+
+    events: {
+      "click #autocompleter-results": "selectResult",
+    },
+
+    initialize: function(){
+      $(this.el).on('keypress', this.handleAutoComplete);
+    },
+
+    // Make an async request to get a matching list of movies
+    // when the autocompleter is receives input (greater than 2 chars)
+    handleAutoComplete: function(){
+      var query = $(this).val();
+      if(query.length < 2)
+        return;
+
+      console.log(query)
+
+      // Make an async call to fetch any possible matches (DO I NEED A ROUTER FOR THIS?)
+      // I DON'T NECESSARILY NEED BACK/FWD HISTORY INFORMATION FOR THIS FEATURE.
+      $.ajax({
+        url: urlMovies + "?term=" + encodeURIComponent(query),
+        dataType: "json",
+        type: "get",
+        success: function (data) {
+          $.map(data['movies'], function(item) {
+
+
+
+            console.log({label: item.title});
+          });
+        }
+      });
+    }
 
   });
+  var autoCompleterView = new AutoCompleterView;
 
-  // Instantiate the **App**.
-  var App = new AppView;
+  // APPVIEW //////////////////////////////////////////////////////////////////
+  // Top level piece of UI. This 'glues' everything (the autocompleter,
+  // the selector and the map) together.
+  /////////////////////////////////////////////////////////////////////////////
+  var AppView = Backbone.View.extend({
+    // This is a simple container for everything ... we don't really need to update it
+    el: $("#container"),
+
+    // At initialization we bind to the relevant events on the `autocomplete input`
+    // Loading any all movie locations from the API.
+    initialize: function(mapView) {
+      this.mapView = mapView;
+    },
+
+    // Re-rendering the App just means
+    //   1. Updating the markers on the map
+    //   2. Clearing the auto-completer
+    //   3. Appending their selected result to the user's `search history`
+    //   4. Update the 'movie info' dom elements on the side bar
+    render: function() {
+      console.log('rendering')
+      this.map.updateMarkers();
+    }
+
+  });
+  var app = new AppView(mapView);
 
 });
