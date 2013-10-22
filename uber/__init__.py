edefe987b95bb7263335e8b9a68e783b61c88abd
cooncesean <cooncesean@gmail.com>
2013-10-22 01:38:08 -0700

    Getting app ready to for deployment on elastic beanstalk. Setting up Raven/Sentry for error logging.

--- uber/__init__.py
+++ uber/__init__.py
@@ -11,6 +11,7 @@ def create_application(default_env='DEVELOPMENT'):
     from flask import Flask
     from flask_environments import Environments
     from flask.ext.mongoengine import MongoEngine
+    from raven.contrib.flask import Sentry
 
     global application
     global env
@@ -23,6 +24,12 @@ def create_application(default_env='DEVELOPMENT'):
 
     # Instantiate the mongo connection
     db = MongoEngine(application)
+
+    # Configure Sentry/error logging
+    sentry = Sentry(
+        application,
+        dsn='https://cf84591bc153450c9a0ca18e35616d5d:3c1ec9c846da43dbb2981f8ce6a1e7db@app.getsentry.com/4404'
+    )
 
     # Import and route all views
     from uber.views import *
